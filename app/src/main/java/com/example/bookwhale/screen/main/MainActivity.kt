86c7903ea5ab0e269be56174ed128ec6bc2554bd
unknown <yeseoplee94@gmail.com>
2022-01-21 19:24:14 +0900

    feat:뒤로가기 버튼 대응

--- app/src/main/java/com/example/bookwhale/screen/main/MainActivity.kt
+++ app/src/main/java/com/example/bookwhale/screen/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bookwhale.screen.main
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.bookwhale.R
 import com.example.bookwhale.databinding.ActivityMainBinding
@@ -9,7 +11,13 @@ import com.example.bookwhale.screen.base.BaseActivity
 import com.example.bookwhale.screen.main.home.HomeFragment
 import com.example.bookwhale.screen.main.favorite.FavoriteFragment
 import com.example.bookwhale.screen.main.my.MyFragment
+import com.example.bookwhale.screen.main.mypost.MyPostFragment
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override val viewModel by viewModel<MainViewModel>()
@@ -17,6 +25,10 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     private var onSearch = false
+
+    private val disposable = CompositeDisposable() // Disposable 관리
+    private val backBtnSubject = PublishSubject.create<Boolean>() // backBtn 이벤트를 발생시킬 수 있는 Subject
+    private val BACK_BTN_EXIT_TIMEOUT = 2000 // 연속된 Back 버튼의 시간 간격 (2초안에 백버튼 2번 클릭시 앱 종료)
 
     override fun initViews(): Unit = with(binding) {
         initBottomNav()
@@ -60,6 +72,7 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
                     true
                 }
                 R.id.menu_myPost -> {
+                    showFragment(MyPostFragment.newInstance(), MyPostFragment.TAG)
                     true
                 }
                 R.id.menu_chat -> {
@@ -99,6 +112,34 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     private fun getSearchArticles(searchText: String) {
         viewModel.getArticles(searchText,PAGE, SIZE)
+    }
+
+    override fun onBackPressed() {
+        disposable.add(backBtnSubject
+            .debounce(100, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Toast.makeText(this, R.string.finishInfo, Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .timeInterval(TimeUnit.MILLISECONDS)
+            .skip(1)
+            .filter { interval ->
+                interval.time() < BACK_BTN_EXIT_TIMEOUT
+            }
+            .subscribe {
+                finishActivity(0)
+                exitProcess(0)
+
+            })
+
+        backBtnSubject.onNext(true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        disposable.clear()
     }
 
     companion object {
